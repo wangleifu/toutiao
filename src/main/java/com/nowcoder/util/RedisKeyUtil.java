5b8a604514a6e680ef8b0d2e3363a68420518aea
Leifu-Wang <1059708873@qq.com>
2018-12-21 18:33:18 +0800

    update util

--- src/main/java/com/nowcoder/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/util/RedisKeyUtil.java
@@ -10,6 +10,11 @@ public class RedisKeyUtil {
     private static String SPLIT = ":";
     private static String BIZ_LIKE = "LIKE";
     private static String BIZ_DISLIKE = "DISLIKE";
+    private static String BIZ_EVENT = "EVENT";
+
+    public static String getEventQueneKey() {
+        return BIZ_EVENT;
+    }
 
     public static String getLikeKey(int entityType, int entityId) {
         return BIZ_LIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
